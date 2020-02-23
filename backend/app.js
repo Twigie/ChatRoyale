f4fa58082b5a63af8a868114daaa954e3d2b82b7
Bran <bpowers.soccer@gmail.com>
2020-02-23 01:09:29 -0500

    update

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,6 @@ const mongoose = require("mongoose");
 const url = "mongodb://chatRoyale:test123#@18.221.11.205:27017";
 const cookieParser = require("cookie-parser");
 
-
-
-
 mongoose.Promise = global.Promise;
 mongoose.connect(url, {useNewUrlParser: true,  useUnifiedTopology: true});
 
@@ -18,12 +15,6 @@ const io = require("socket.io")(server);
 
 io.serveClient("origins", "*:*");
 const namespaces = [];
-
-
-
-
-
-
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -38,12 +29,26 @@ const port = process.env.PORT || 5000;
 const users = require("./routes/users");
 app.use("/api/users", users);
 
-const games = require("./routes/games");
-app.use("/api/games", games);
+app.post("/api/games/create", (req, res) => {
+    console.log(req.body);
+    let roomName = req.body.roomID;
+    let roomObj = {}
+    roomObj.lobby = io.of(req.body.name);
+    roomObj.id = roomName;
+    namespaces.push(roomObj);
+    res.send("created server");
+})
+
+app.get("/api/games/list", (req, res) => {
+    console.log(namespaces);
+    res.send("namespace list");
+})
+
+
 
 
 app.listen(port, () => console.log(`server started on port ${port}`));
 module.exports = {
     namespaces,
-    app
+    io
 }
